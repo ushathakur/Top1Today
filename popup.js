117bbc17a1c383aa9f02f8f1e84b2e52176394ff
ushathakur <55362982+ushathakur@users.noreply.github.com>
2020-11-16 15:54:38 -0500

    changes made to message displayed on UI
    shows current website's name when clicked on the button.

--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 function click(){
 chrome.tabs.query({ active: true }, function (tab) {
     var tabUrl = tab[0].url;
+   
     
-    document.getElementById('demo').textContent =`your current website is ${tabUrl}` ;
+    document.getElementById('demo').textContent =`your Top1Today website is ${tabUrl} Time Spent: 3 Hrs` ;
     return tabUrl;
 });
 }
